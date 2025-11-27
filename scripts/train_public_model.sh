1643315c5bed3fe13b80de529be61ec9b1bb3fc8
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-11-27 19:04:14 +0100

    scripts for training different sizes (including tiny)

--- scripts/train_public_model.sh
+++ scripts/train_public_model.sh
@@ -1,10 +1,11 @@
 # This will read stream data from the public endpoints by default, but that might be a lot slower
 # than reading data locally.
+export MODEL_SIZE="tiny" # tiny or 7B
 export DATA_ROOT="http://flexolmo-data.org"
-export CHECKPOINTS=  # /path/to/checkpoints
+export CHECKPOINTS=checkpoints  # /path/to/checkpoints
 
 # Train base model on public mix for 1T tokens (schedule set to 5T)
-torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-7B-train.py OLMo2-7B-public-mix \
+torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-${MODEL_SIZE}-train.py OLMo2-${MODEL_SIZE}-public-mix \
     --dataset.mix_base_dir=${DATA_ROOT} \
     --dataset.mix=public_mix \
     --train_module.float8_config.enabled=true \
@@ -12,12 +13,12 @@ torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-7B-train.py OLMo2-7B-publi
     --trainer.duration.unit=tokens \
     --trainer.hard_stop.value=1_000_000_000_000 \
     --trainer.hard_stop.unit=tokens \
-    --trainer.save_folder=${CHECKPOINTS}/OLMo2-7B-public-mix
+    --trainer.save_folder=${CHECKPOINTS}/OLMo2-${MODEL_SIZE}-public-mix
 
 # Anneal for 50B tokens, using the checkpoint from the base model training.
 # Make sure to set the correct path to the checkpoint.
-BASE_CHECKPOINT=${CHECKPOINTS}/OLMo2-7B-public-mix/step238419
-torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-7B-anneal.py OLMo2-7B-anneal-public-mix ${BASE_CHECKPOINT}
+BASE_CHECKPOINT=${CHECKPOINTS}/OLMo2-${MODEL_SIZE}-public-mix/step238419
+torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-anneal.py OLMo2-${MODEL_SIZE}-anneal-public-mix ${BASE_CHECKPOINT}
     --dataset.mix_base_dir=${DATA_ROOT} \
     --dataset.mix=public_mix \
     --train_module.float8_config.enabled=true \
@@ -26,9 +27,9 @@ torchrun --nproc-per-node=8 ./src/scripts/train/OLMo2-7B-anneal.py OLMo2-7B-anne
     --trainer.save_folder=${CHECKPOINTS}/OLMo2-7B-anneal-public-mix
 
 # Unshard the final checkpoint
-MODEL_PATH=${CHECKPOINTS}/OLMo2-7B-anneal-public-mix/step11921
+MODEL_PATH=${CHECKPOINTS}/OLMo2-${MODEL_SIZE}-anneal-public-mix/step11921
 python src/scripts/utils/unshard.py -i ${MODEL_PATH} -o ${MODEL_PATH}-unsharded
 
-# Upcycle the dense model to 2x7B (when not using domain-specific embeddings)
-# MODEL_PATH=${CHECKPOINTS}/OLMo2-7B-anneal-public-mix/step11921
-# python src/scripts/upcycle/dense_to_expert_moe.py -m ${MODEL_PATH}-unsharded ${MODEL_PATH}-unsharded -t ${CHECKPOINTS}/olmoe-2x7b-public-public
+# Upcycle the dense model to 2xMODEL_SIZE (when not using domain-specific embeddings)
+MODEL_PATH=${CHECKPOINTS}/OLMo2-${MODEL_SIZE}-anneal-public-mix/step11921
+python src/scripts/upcycle/dense_to_expert_moe.py -m ${MODEL_PATH}-unsharded ${MODEL_PATH}-unsharded -t ${CHECKPOINTS}/olmoe-2x${MODEL_SIZE}-public-public
---
+++ src/scripts/train/OLMo2-anneal.py
@@ -0,0 +1,118 @@
+"""
+Meant to be launched with torchrun.
+"""
+
+import logging
+import os
+import sys
+
+from olmo_core.data import (
+    NumpyDatasetConfig,
+)
+from olmo_core.nn.transformer import (
+    TransformerConfig,
+)
+from olmo_core.optim import AdamWConfig, CosWithWarmup
+from olmo_core.train import (
+    TrainerConfig,
+    prepare_training_environment,
+    teardown_training_environment,
+)
+from olmo_core.train.train_module import (
+    TransformerTrainModuleConfig,
+)
+from rich import print
+
+from flexolmo.internal.common import (
+    CommonComponents,
+    build_experiment_config,
+    get_root_dir,
+    is_dry_run,
+    print_model_params,
+)
+from flexolmo.internal.model_utils import *  # noqa
+from flexolmo.internal.train_utils import anneal
+
+log = logging.getLogger(__name__)
+
+SEQUENCE_LENGTH = 4096
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def build_model_config_factory(model_size: str):
+    if model_size.lower() == "tiny":
+        def build_model_config(common: CommonComponents):
+            return TransformerConfig.olmo2_tiny(
+                vocab_size=256,
+                n_layers=1,
+                n_heads=1,
+            )
+    elif model_size.lower() == "7B":
+        def build_model_config(common: CommonComponents):
+            return TransformerConfig.olmo2_7B(vocab_size=common.tokenizer.padded_vocab_size())
+    return build_model_config
+
+
+def build_dataset_config(common: CommonComponents) -> NumpyDatasetConfig:
+    from flexolmo.data.mixes import CustomDataMix
+
+    dataset_config = common.dataset
+    dataset_config.mix = CustomDataMix.public_mix
+    return dataset_config
+
+
+def build_trainer_config(common: CommonComponents) -> TrainerConfig:
+    trainer_config = common.trainer
+    # Add any changes to the trainer configuration here
+    return trainer_config
+
+
+def build_train_module_config(common: CommonComponents) -> TransformerTrainModuleConfig:
+    train_module_config = common.train_module
+    train_module_config.rank_microbatch_size = 2 * SEQUENCE_LENGTH
+    train_module_config.scheduler = CosWithWarmup(warmup_steps=2000)
+    train_module_config.state_dict_save_opts = {"flatten_optimizer_state_dict": True}
+
+    assert isinstance(train_module_config.optim, AdamWConfig)
+    train_module_config.optim.lr = 9e-4
+    return train_module_config
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    if len(sys.argv) < 2:
+        print(
+            f"Usage: torchrun [OPTS..] {sys.argv[0]} [dry_run] model_size run_name checkpoint [OVERRIDES...]"
+        )
+        sys.exit(1)
+
+    dry_run = is_dry_run(sys.argv)
+
+    if dry_run:
+        _, model_size, run_name, checkpoint, *overrides = sys.argv[1:]
+    else:
+        model_size, run_name, checkpoint, *overrides = sys.argv[1:]
+        prepare_training_environment()
+
+    try:
+        build_model_config = build_model_config_factory(model_size)
+        config = build_experiment_config(
+            run_name,
+            overrides,
+            root_dir=get_root_dir(),
+            sequence_length=SEQUENCE_LENGTH,
+            global_batch_size=1024 * SEQUENCE_LENGTH,
+            include_default_evals=True,
+            freeze_embeddings=False,
+            model_config_builder=build_model_config,
+            dataset_config_builder=build_dataset_config,
+            trainer_config_builder=build_trainer_config,
+            train_module_config_builder=build_train_module_config,
+        )
+        print(config)
+        print_model_params(config)
+        if dry_run:
+            sys.exit(0)  # Exit early for dry run
+        anneal(checkpoint, config)
+    finally:
+        teardown_training_environment()
